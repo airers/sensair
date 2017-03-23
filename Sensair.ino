ce0614b5835d54d5d3624fa58af628cc77251238
Jia Xun <chaijiaxun@gmail.com>
2017-03-24 00:41:06 +0800

    Initial commit

---
+++ Sensair.ino
@@ -0,0 +1,14 @@
+//Including libraries to connect RTC by Wire and I2C
+#include <Wire.h>
+#include "RTClib.h"
+#include <SD.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
