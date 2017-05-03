12bcb3f2bd7d51dfddacfffc1b39dac37a90b64e
Jia Xun <chaijiaxun@gmail.com>
2017-05-03 20:26:16 +0800

    Fully ported, life is good

--- sensair.ino
+++ sensair.ino
@@ -6,23 +6,22 @@
 // Including library for DHT22 (Temp and RH sensor)
 #include <DHT.h>
 
-//Including library for GPS
+// Including library for GPS
 // #include <TinyGPS++.h>
 
-//Including libraries to connect RTC by Wire and I2C
+// Including libraries to connect RTC by Wire and I2C
 // #include <Wire.h>
 // #include <RTClib.h>
 #include <SoftwareSerial.h>
-//
-// #include "classes/CommandProcessor.h"
-// #include "classes/CommandProcessor.cpp"
+#include "classes/CommandProcessor.h"
+#include "classes/CommandProcessor.cpp"
 #include "classes/StateManager.h"
-// #include "classes/FileProcessor.h"
+#include "classes/FileProcessor.h"
 #include "classes/EEPROMVariables.h"
 
 // Temp used for testing
-#include <SD.h>
-#include <SPI.h>
+// #include <SD.h>
+// #include <SPI.h>
 // #include <DS1307RTC.h>
 // #include <Time.h>
 #include <Wire.h>
@@ -50,7 +49,6 @@ DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal
 
 //Defining pins for TFT Screen
 
-
 // LED pins
 #define POWER_LED 6
 #define WORKING_LED 5
@@ -62,8 +60,6 @@ DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal
 // RST   RST     9
 // CS    CS      8
 
-// #define TFT_SCLK   21
-// #define TFT_MOSI   22
 #define TFT_SCLK   12
 #define TFT_MOSI   11
 #define TFT_DC     10
@@ -72,21 +68,18 @@ DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal
 
 #define BTSerial Serial1
 
-
-
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 SoftwareSerial btSerial(BLUETOOTH_RX, BLUETOOTH_TX);
 StateManager stateManager;
-// FileProcessor fileProcessor;
+FileProcessor fileProcessor;
 
-//Defining pins for GPS
+// Defining pins for GPS
 // SoftwareSerial gpsSerial(3, 2); //RX=pin 10, TX=pin 11
 // TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
 
 // unsigned int samplingTime = 280; //Datasheet: Duration before measuring the ouput signal (after switching on LED): 280 µs
 // unsigned int deltaTime = 40; //Datasheet: Duration of the whole excitation pulse: 320 µs; Duration before switching off LED: 40 µs
 // unsigned int sleepTime = 9680; //Datasheet: Pulse Cycle: 10ms; Remaining time: 10,000 - 320 = 9680 µs
-
 
 long calculateNextMinute() {
   DateTime currentDateTime(ROMVar::getCurrentTime());
@@ -111,8 +104,10 @@ void setup() {
 
   stateManager.init();
   stateManager.printNow();
+  fileProcessor.init();
 
-  // fileProcessor.init();
+  Serial.print("First reading: ");
+  Serial.println(fileProcessor.getFirstReading());
 
   // pinMode(LED_POWER_PIN ,OUTPUT); //Configures the digital pin as an output (to set it at 0V and 5V per cycle; turning on and off the LED
   // pinMode(10, OUTPUT); //Configures the pin of the SD card reader as an output
@@ -129,6 +124,7 @@ void setup() {
    * This will result in reading loses of 2 seconds.
    * This is not a problem, just something to take note.
    */
+
   // uint16_t count = fileProcessor.countPackets2(1490830040);
   // Serial.println(count);
   // count = fileProcessor.countPackets2(1490830040);
@@ -141,15 +137,15 @@ void setup() {
   // Serial.println(fileProcessor.getFirstReading());
 
   // pinMode(20, INPUT_PULLUP); //Configures the pin of the SD card reader as an output
-  if (!SD.begin(20)) {
-    Serial.println("NO SD");
-  } else {
-    Serial.println("SD Available");
-    File currentDayFile = SD.open("TEST.TXT", FILE_WRITE);
-    currentDayFile.println("Testing stuff");
-    currentDayFile.close();
-  }
 
+  // if (!SD.begin(20)) {
+  //   Serial.println("NO SD");
+  // } else {
+  //   Serial.println("SD Available");
+  //   File currentDayFile = SD.open("TEST.TXT", FILE_WRITE);
+  //   currentDayFile.println("Testing stuff");
+  //   currentDayFile.close();
+  // }
 
   Serial.println("Start");
 
@@ -171,53 +167,81 @@ void setup() {
   digitalWrite(WORKING_LED, LOW);
   digitalWrite(POWER_LED, HIGH);
 
+  // Error messages
+  if ( !fileProcessor.getCardAvailable() ) {
+    tft.setTextColor(ST7735_WHITE);
+    tft.setCursor(30,30);
+    tft.setTextSize(2);
+    tft.println("NO SD");
+    tft.setTextSize(1);
+  }
 }
 
-
 void loop() {
+  // Error messages
+  if ( !fileProcessor.getCardAvailable() ) {
+    delay(500);
+    digitalWrite(POWER_LED, HIGH);
+    delay(500);
+    digitalWrite(POWER_LED, LOW);
+    return;
+  }
+
+  digitalWrite(WORKING_LED, LOW);
+
   // Process the incoming bluetooth packets
   {
-    byte type = 0;
-    uint8_t len = 0;
-    bool read = false;
-    if ( btSerial.available() ) {
-      Serial.println("Received packet");
-    }
-    while(btSerial.available()) {
-      Serial.print(btSerial.read());
-      Serial.print(" ");
-      read = true;
-    }
-    if ( read )
-    Serial.println();
 
-    btSerial.write(CMD_CONNECTION_ACK);
-    int packet_len = 4;
-    btSerial.write(packet_len);
-    long_u timestamp;
-    timestamp.data = stateManager.getTimeStamp();
-    btSerial.write(timestamp.bytes[0]);
-    btSerial.write(timestamp.bytes[1]);
-    btSerial.write(timestamp.bytes[2]);
-    btSerial.write(timestamp.bytes[3]);
-    btSerial.print("\r\n");
+      byte type = 0;
+      uint8_t len = 0;
+      bool read = false;
+      if ( btSerial.available() ) {
+        Serial.println("Received packet");
+      }
+      while ( btSerial.available() ) {
+         type = btSerial.read();
+         if ( btSerial.available() ) {
+           len = btSerial.read();
+         }
+         byte data [len];
+         int i = 0;
+         while ( i < len && btSerial.available() ) {
+           data[i] = btSerial.read();
+           i++;
+         }
+         CommandProcessor::processPacket(type, len, data, btSerial, stateManager, fileProcessor);
+       }
+      // while(btSerial.available()) {
+      //   Serial.print(btSerial.read());
+      //   Serial.print(" ");
+      //   read = true;
+      // }
+      // if ( read )
+      // Serial.println();
 
-    // while ( BTSerial.available() ) {
-    //   type = BTSerial.read();
-    //   if ( BTSerial.available() ) {
-    //     len = BTSerial.read();
-    //   }
-    //   byte data [len];
-    //   int i = 0;
-    //   while ( i < len && BTSerial.available() ) {
-    //     data[i] = BTSerial.read();
-    //     i++;
-    //   }
-    //
-    //   Serial.println(i);
-    //   // CommandProcessor::processPacket(type, len, data, btSerial, stateManager, fileProcessor);
-    // }
   }
+  // {
+  //   byte type = 0;
+  //   uint8_t len = 0;
+  //   bool read = false;
+  //   if ( btSerial.available() ) {
+  //     Serial.println("Received packet");
+  //   }
+  //
+  //   while ( btSerial.available() ) {
+  //     type = btSerial.read();
+  //     if ( btSerial.available() ) {
+  //       len = btSerial.read();
+  //     }
+  //     byte data [len];
+  //     int i = 0;
+  //     while ( i < len && btSerial.available() ) {
+  //       data[i] = btSerial.read();
+  //       i++;
+  //     }
+  //     CommandProcessor::processPacket(type, len, data, btSerial, stateManager, fileProcessor);
+  //   }
+  // }
 
   // Process GPS packets
   {
@@ -267,14 +291,14 @@ void loop() {
       //Read data and store it to variables hum and temp
       hum = dht.readHumidity(); // Relative Humidity in %
       temp = dht.readTemperature(); // Temperature in deg C
-      Serial.print("Hum: ");
-      Serial.print(hum);
-      Serial.print("% Temp: ");
-      Serial.print(temp);
-      Serial.print("C");
-      Serial.print(" Dust: ");
-      Serial.println(dustDensity);
-      stateManager.printNow();
+      // Serial.print("Hum: ");
+      // Serial.print(hum);
+      // Serial.print("% Temp: ");
+      // Serial.print(temp);
+      // Serial.print("C");
+      // Serial.print(" Dust: ");
+      // Serial.println(dustDensity);
+      // stateManager.printNow();
 
       {
         //Printing data onto TFT
@@ -316,26 +340,27 @@ void loop() {
       }
 
       // TODO: Not hardcode the microclimate and location
-      // fileProcessor.pushData(dustDensity, 1.35432101, 103.8765432, 0);
+      fileProcessor.pushData(dustDensity, 1.35432101, 103.8765432, 0);
+      // long prevMinuteTime = nextMinuteTime - 60;
+      // fileProcessor.openAppropiateFile(currentTime);
+      // fileProcessor.storeAverageData(currentTime, stateManager.microclimate);
+
     }
     if ( currentTime >= nextMinuteTime ) {
       long prevMinuteTime = nextMinuteTime - 60;
 
       // Average past minute readings & save as previous minute
-      // fileProcessor.openAppropiateFile(prevMinuteTime);
-      // fileProcessor.storeAverageData(prevMinuteTime, stateManager.microclimate);
+      fileProcessor.openAppropiateFile(prevMinuteTime);
+      fileProcessor.storeAverageData(prevMinuteTime, stateManager.microclimate);
       nextMinuteTime = calculateNextMinute();
       ROMVar::setNextMinuteTime(nextMinuteTime);
 
     }
 
-
-    //
-
   }
 
   // Send packets if there's something requesting it
-  // fileProcessor.sendSomePackets(btSerial);
+  fileProcessor.sendSomePackets(btSerial);
 
   /**
    * Arduino date format
@@ -378,6 +403,10 @@ void loop() {
   //   }
   //   delay(9000);
   // }
-
+  if ( fileProcessor.packetsToSend > 0 ) {
+    Serial.print("Sending: ");
+    Serial.println(fileProcessor.packetsToSend);
+    digitalWrite(WORKING_LED, HIGH);
+  }
   delay(50);
 }
